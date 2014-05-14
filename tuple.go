19535952e3362402ad24b15d126a97f0d7f1c585
Kevin Manley <kevin.manley+github@gmail.com>
2014-05-13 22:08:04 -0400

    added comments

--- tuple.go
+++ tuple.go
@@ -9,18 +9,21 @@ type Tuple struct {
 	data []interface{}
 }
 
+// Creates a new empty Tuple of length n
 func NewTuple(n int) *Tuple {
 	t := &Tuple{}
 	t.data = make([]interface{}, n)
 	return t
 }
 
+// Creates a new Tuple from an existing slice
 func NewTupleFromSlice(slice []interface{}) *Tuple {
 	t := &Tuple{}
 	t.data = slice
 	return t
 }
 
+// Creates a new tuple from a literal sequence of items
 func NewTupleFromItems(items ...interface{}) *Tuple {
 	t := NewTuple(len(items))
 	for i, item := range items {
@@ -29,14 +32,18 @@ func NewTupleFromItems(items ...interface{}) *Tuple {
 	return t
 }
 
+// Returns the number of elements in the Tuple
 func (this *Tuple) Len() int {
 	return len(this.data)
 }
 
+// Returns the internal slice
 func (this *Tuple) Slice() []interface{} {
 	return this.data
 }
 
+// Convert n to an index into the internal slice.
+// Negative numbers are supported, e.g. -1 points to the last item
 func (this *Tuple) Index(n int) int {
 	// allow negative indexing as in Python
 	if n < 0 {
@@ -45,19 +52,24 @@ func (this *Tuple) Index(n int) int {
 	return n
 }
 
+// Set the item at index n
 func (this *Tuple) Set(n int, item interface{}) {
 	this.data[this.Index(n)] = item
 }
 
+// Get the item at index n
 func (this *Tuple) Get(n int) interface{} {
 	item := this.data[this.Index(n)]
 	return item
 }
 
+// Returns a string representation of the Tuple
 func (this *Tuple) String() string {
 	return fmt.Sprintf("%v", this.data)
 }
 
+// Pops the leftmost item from the Tuple and
+// returns it
 func (this *Tuple) PopLeft() interface{} {
 	if this.Len() < 1 {
 		return nil
@@ -67,6 +79,8 @@ func (this *Tuple) PopLeft() interface{} {
 	return ret
 }
 
+// Pops the rightmost item from the Tuple and
+// returns it
 func (this *Tuple) PopRight() interface{} {
 	if this.Len() < 1 {
 		return nil
@@ -77,12 +91,14 @@ func (this *Tuple) PopRight() interface{} {
 	return ret
 }
 
+// Reverses the Tuple in place
 func (this *Tuple) Reverse() {
 	for i, j := 0, this.Len()-1; i < j; i, j = i+1, j-1 {
 		this.data[i], this.data[j] = this.data[j], this.data[i]
 	}
 }
 
+// Returns True if this Tuple is elementwise equal to other
 func (this *Tuple) Eq(other *Tuple) bool {
 	if this.Len() != other.Len() {
 		return false
@@ -95,10 +111,12 @@ func (this *Tuple) Eq(other *Tuple) bool {
 	return true
 }
 
+// Returns True if this Tuple is not elementwise equal to other
 func (this *Tuple) Ne(other *Tuple) bool {
 	return !this.Eq(other)
 }
 
+// Returns True if this Tuple is elementwise less than other
 // TODO: use Sortable (sp?) interface instead
 func (this *Tuple) Lt(other *Tuple) bool {
 	tlen, olen := this.Len(), other.Len()
@@ -120,14 +138,19 @@ func (this *Tuple) Lt(other *Tuple) bool {
 	return false
 }
 
+// Returns True if this Tuple is elementwise less than
+// or equal to other
 func (this *Tuple) Le(other *Tuple) bool {
 	return this.Lt(other) || this.Eq(other)
 }
 
+// Returns True if this Tuple is elementwise greater than other
 func (this *Tuple) Gt(other *Tuple) bool {
 	return !this.Le(other)
 }
 
+// Returns True if this Tuple is elementwise greater than
+// or equal to other
 func (this *Tuple) Ge(other *Tuple) bool {
 	return !this.Lt(other)
 }
